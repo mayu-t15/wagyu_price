2426a67eb0f9a47f23b293fc71e79a9e5597527a
高橋茉由 <>
2021-02-12 15:06:55 +0900

    モバイルサイズの時のスムーススクロール実装

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('a[id = "back-to-top"]').click(function() {
+    var adjust = 0;
+    var speed = 1000;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  })
+})
